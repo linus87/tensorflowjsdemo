8b0419c5d5ed6b482e65eafe9ad23a95a9520294
linus <lyan2@ebay.com>
2024-03-18 17:49:57 +0800

    update input and dense demo

--- src/dense_layer/src/index.js
+++ src/dense_layer/src/index.js
@@ -1,20 +1,28 @@
 // Tiny TFJS train / predict example.
+let inputLayer, denseLayer;
 async function run() {
     // Create a simple model.
     const model = tf.sequential();
     
-    // Calling layers.inputLayer() using add() method 
-    model.add(tf.layers.inputLayer({inputShape: [4]})); 
+    // Create a input layer node . Input shape is [none, 4]
+    inputLayer = tf.layers.inputLayer({inputShape: [2, 4], batchSize: 2});
+    model.add(inputLayer); 
 
     // kernelInitializer default value is glorot_uniform, biasInitializer default value is zeros.
-    model.add(tf.layers.dense({units: 1, kernelInitializer:"ones", useBias: true}));
+    // Here, the kernel is ones, output shape is [1, 1]
+
+    denseLayer = tf.layers.dense({units: 1, kernelInitializer:"ones", useBias: true});
+    
+    model.add(denseLayer);
+
+    model.summary();
 
     /* Output is a tensor of shape [1, 4], it will render in the browser console as: 
      * Tensor
-     * [[4],]
+     * [[4],
+     *  [4]]
      */
-    model.predict(tf.ones([1, 4]), {batchSize:1}).print();
-
+    model.predict(tf.ones([1, 2, 4])).print();
   }
   
   run();
--- src/input_layer/src/index.js
+++ src/input_layer/src/index.js
@@ -3,14 +3,18 @@ async function run() {
     // Create a simple model.
     const model = tf.sequential();
     
-    // Calling layers.inputLayer() using add() method 
-    model.add(tf.layers.inputLayer({inputShape: [4]}));
+    // create a input layer node, input shape is [none, 4]
+    model.add(tf.layers.inputLayer({inputShape: [4], batchSize: 2}));
 
-    /* Output is a tensor of shape [1, 4], it will render in the browser console as: 
+    model.summary();
+
+    /* Output is a tensor of shape [2, 4], it will render in the browser console as: 
      * Tensor
-     * [[1, 1, 1, 1],]
+     * [[1, 1, 1, 1],
+     *  [1, 1, 1, 1],
+     *  [1, 1, 1, 1]]
      */
-    model.predict(tf.ones([1, 4]), {batchSize:1}).print();
+    model.predict(tf.ones([3, 4]), {batchSize:2}).print();
 
   }
   
